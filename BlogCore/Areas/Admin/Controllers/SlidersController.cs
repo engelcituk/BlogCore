c8f7485d8c76e967833f998d468d7d607c352acb
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-07 17:54:44 -0500

    vista index, y vista crear para sliders

---
+++ BlogCore/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogCore.AccesoDatos.Data.Repository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SlidersController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+        private readonly IWebHostEnvironment _hostingEnviroment;
+
+        public SlidersController(IContenedorTrabajo contenedorTrabajo, IWebHostEnvironment hostingEnviroment)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+            _hostingEnviroment = hostingEnviroment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        #region llamadas a la api
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Slider.GetAll() });
+        }
+        #endregion
+    }
+}
---
+++ BlogCore/wwwroot/js/slider.js
@@ -0,0 +1,59 @@
+﻿let datatable;
+
+$(document).ready( ()=> {
+    cargarDatatable();
+});
+
+function cargarDatatable() {
+    datatable = $("#tblSliders").DataTable({
+        "ajax": {
+            "url": "/admin/sliders/GetAll",
+            "type": "GET",
+            "datatype":"json"
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "nombre", "width": "35%" },
+            { "data": "estado", "width": "20%" },           
+            {
+                "data": "id",
+                "render": (data) => {
+                    return `<div class="text-center">
+                                <a href='/Admin/Sliders/Edit/${data}' class='btn btn-success text-white' style='cursor:pointer; '> <i class='fas fa-edit'></i></a>&nbsp
+                                <button onclick=Delete("/Admin/Sliders/Delete/${data}")  class='btn  btn-danger text-white' style='cursor:pointer; '><i class='fas fa-trash'></i></button>
+                            </idv>`;
+                },"width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No hay registros" 
+        },
+        "width":"100%"
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "¿Está seguro de borrar?",
+        text: "Este contenido no se podrá recuperar",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Sí, borrar",
+        closeOnConfirm: true
+    },
+    function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    datatable.ajax.reload();
+                } else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
+}
