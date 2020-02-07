d805ce30f510b792531eb5962933a59b59762829
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-07 11:39:05 -0500

    Crear controlador Articulo,ViewModel y método y vista Index

---
+++ BlogCore/Areas/Admin/Controllers/ArticulosController.cs
@@ -0,0 +1,35 @@
+﻿
+using BlogCore.AccesoDatos.Data.Repository;
+using BlogCore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticulosController : Controller
+    {
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public ArticulosController(IContenedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+        public IActionResult Index()
+        {
+            ArticuloVM articuloVm = new ArticuloVM()
+            {
+                Articulo = new Models.Articulo(),
+                ListaCategorias = _contenedorTrabajo.Categoria.GetListaCategorias()
+            };
+            return View(articuloVm);
+        }
+
+        #region llamadas a la api
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _contenedorTrabajo.Articulo.GetAll(includeProperties:"Categoria") });
+        }        
+        #endregion
+    }
+}
